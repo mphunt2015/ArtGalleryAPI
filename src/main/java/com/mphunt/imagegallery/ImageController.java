b300f6b5e7d23b3112653a426e47f1b692672744
mphunt2015 <mphunt@us.ibm.com>
2025-06-02 15:44:33 -0500

    Added real artwork for Image Gallery app

--- src/main/java/com/mphunt/imagegallery/ImageController.java
+++ src/main/java/com/mphunt/imagegallery/ImageController.java
@@ -13,9 +13,16 @@ public class ImageController {
     @GetMapping
     public List<ImageDTO> getImages() {
         return List.of(
-                new ImageDTO("Students", "https://www.ucl.ac.uk/bartlett/sites/bartlett/files/styles/department_hero_mobile/public/2025-04/IIPP%20MPA%20-%20240205_J-TYE_UCL_209_Bartlett_3031.jpg.jpg?itok=ujh4sNOw"),
-                new ImageDTO("Collaboration", "https://www.mpg.de/11722646/header_image-1653994062.jpg?t=eyJ3aWR0aCI6ODQ4LCJmaWxlX2V4dGVuc2lvbiI6ImpwZyIsIm9ial9pZCI6MTE3MjI2NDZ9--6c7113e80443d020de403f3f92c0b6cc175f4fbc"),
-                new ImageDTO("Talking", "https://aspph-redesign-wp-production.s3.amazonaws.com/wp-content/uploads/2025/04/54454151678_69e7852494_o-1024x683.jpg")
+                new ImageDTO("Poker Game", "https://www.theartist.me/wp-content/uploads/2021/05/download-4-1.jpg"),
+                new ImageDTO("Rainy Day, Boston", "https://www.theartist.me/wp-content/uploads/2021/05/1280px-Childe_Hassam_-_Rainy_Day_Boston_-_Google_Art_Project-1024x547.jpg"),
+                new ImageDTO("Water Lillies and the Japanese Bridge", "https://www.theartist.me/wp-content/uploads/2021/05/water-lily-pond-with-japanese-bridge.jpg"),
+                new ImageDTO("The Drowning Girl", "https://www.theartist.me/wp-content/uploads/2021/05/6530203451_43ef56b1a5_b.jpg"),
+                new ImageDTO("The Boating Party", "https://www.theartist.me/wp-content/uploads/2021/05/the_boating_party_1963.10.94-2048x1579.jpg"),
+                new ImageDTO("The Cracked Cardinal", "https://www.theartist.me/wp-content/uploads/2021/05/1200px-George_Condo_The_Cracked_Cardinal-843x1024.jpg"),
+                new ImageDTO("Composition VII", "https://www.theartist.me/wp-content/uploads/2021/05/36-1536x1098.jpg"),
+                new ImageDTO("Victory Boogie Woogie", "https://www.theartist.me/wp-content/uploads/2021/05/cri_000000477212-1024x1021.jpg"),
+                new ImageDTO("The Scream", "https://www.theartist.me/wp-content/uploads/2021/05/the-scream-by-edvard-munch-a-critical-analysis-1024x576.jpg"),
+                new ImageDTO("Disintegration of the Persistence of Memory", "https://www.theartist.me/wp-content/uploads/2021/05/8029c23eb07c4f81101ce8c66daf2421-1536x1193.jpg")
         );
     }
 }
